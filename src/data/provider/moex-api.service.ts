0f8181d0b50cc84e58b98588e33addd2cd8a62aa
emaldie <trueyamuxa223@gmail.com>
2024-12-11 05:32:29 +0300

    получение динамики цен каждой секьюрити

--- src/data/provider/moex-api.service.ts
+++ src/data/provider/moex-api.service.ts
@@ -21,4 +21,29 @@ export class MoexApiService {
         }
         return result;
     }
+
+    async getMoexSecurities (securitiesAmount = 300) {
+        const response = await axios.get("https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json?securities.columns=SECID,SHORTNAME");
+        const data = response.data;
+        const securities = [];
+        data.securities.data.forEach(element => {
+            securities.push(element[0]);
+        });
+        return securities;
+    }
+
+    async getHistoricalData (securities: string[], daysAmount: number = 100) {
+        const historicalData = {};
+        for (const security of securities) {
+            const response = await axios.get(`https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/${security}.json?from=2023-12-10&till=2024-12-10`);
+            const data = response.data;
+            const prices = [];
+            data.history.data.forEach(item => {
+                const closePrice = +item[9];
+                prices.push(closePrice);
+            });
+            historicalData[security] = prices;
+        }
+        return historicalData;
+    }    
 }
