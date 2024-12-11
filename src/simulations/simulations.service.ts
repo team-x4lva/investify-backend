27399aee92728c5cdc27cce003a0f47834eddd4d
emaldie <trueyamuxa223@gmail.com>
2024-12-11 12:28:23 +0300

    скелет генерации симуляции

--- src/simulations/simulations.service.ts
+++ src/simulations/simulations.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { GeminiAIService } from "../data/processor/ai/gemini-ai.service";
 import { ScraperService } from "src/data/provider/scraper.service";
+import { portfolioItem, SimulationPromptParameters } from "./interfaces/simulation-prompt-parameters.interface";
+import { PortfoliosService } from '../portfolios/portfolios.service';
 
 @Injectable()
 export class SimulationsService {
     constructor(
         private readonly scraperService: ScraperService,
-        private readonly geminiAIService: GeminiAIService
+        private readonly geminiAIService: GeminiAIService,
+        private readonly portfoliosService: PortfoliosService
     ) {}
 
     async conductSimulation(
@@ -14,13 +17,25 @@ export class SimulationsService {
         endDate: Date,
         portfolioId: number
     ) {
+        const portfolio = await this.portfoliosService.findOne(portfolioId);
+        const items: portfolioItem[] = [];
+        for(let i = 0; i < portfolio.securities.length; i++) {
+            items.push({
+                security: portfolio.securities[i],
+                share: portfolio.share[i]
+            })
+        }
         const bestDepositRates =
             await this.scraperService.getBestDepositRates();
-
-        //this.geminiAIService.();
-
+        const promptParameters: SimulationPromptParameters = {
+            moneyAmount: moneyAmount,
+            startDate: new Date(),
+            endDate: endDate,
+            portfolio: items
+        }
+        const result = await this.geminiAIService.generateSimulation(promptParameters);
         return {
-            bestDepositRates
+            bestDepositRates, result
         };
     }
 }
